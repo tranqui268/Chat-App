0ebb3002af7430473506c084ea7687735db9fc60
tranqui268 <trannhatqui712@gmail.com>
2024-04-24 14:53:26 +0700

    first commit

---
+++ QChat/app/src/main/java/com/example/qchat/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.qchat.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.qchat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
