a450d8e5894b67e3e19d89cf9edbd8cbe3e46927
tranqui268 <trannhatqui712@gmail.com>
2025-03-09 13:54:01 +0700

    feat chat ui

--- QChat/lib/screen/message_page.dart
+++ QChat/lib/screen/message_page.dart
@@ -23,9 +23,81 @@ class MessagePage extends StatelessWidget{
             )
         ],
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Container(
+            height: 100,
+            padding: const EdgeInsets.all(5),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                _buildRecentContact("test","https://res.cloudinary.com/dhis8yzem/image/upload/v1741011247/chatApp/Avatar_default.png","abc",true,context)
+              ],
+
+            ),
+          )
+        ]
+      ),
 
       
+    );
+  }
+
+  Widget _buildRecentContact(String name, String url, String note, bool isOnline, BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              // Avatar 
+              CircleAvatar(
+                radius: 30,
+                backgroundImage: NetworkImage(url),
+              ),
+              
+              // Bong bóng chat
+              if(note.isNotEmpty)
+              Positioned(
+                top: 0,
+                left: 10,
+                child: GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (_) => ForgotPassword()));
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.6),
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: Text(note, style: const TextStyle(color: Colors.white, fontSize: 12)),
+                  ) ,
+                ),
+              ),
+
+              if(isOnline)
+              Positioned(
+                bottom: 5,
+                right: 5,
+                child: Container(
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 2)
+                  ),
+                )
+              )
+
+            ],
+          ),
+          const SizedBox(height: 5,),
+          Text(name)
+
+        ],
+      ),
     );
   }
   
